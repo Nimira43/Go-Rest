761ca111dcda16aac7c5d148e0b8ffc76c784ae9
Nimira43 <n.rabone72@aol.com>
2024-09-15 16:12:21 +0100

    Setting up first route and handling first request.

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default() 
+	server. GET("/events", getEvents)
+
+	server.Run(":8080") // localhost
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+}                                                                                                  
